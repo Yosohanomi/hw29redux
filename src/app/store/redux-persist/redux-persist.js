bda579d082e4c4dda33614af18ccdd9a05f89f42
Лисенко Софія <146013576+Yosohanomi@users.noreply.github.com>
2026-05-09 14:54:38 +0300

    Update redux-persist.js

--- src/app/store/redux-persist/redux-persist.js
+++ src/app/store/redux-persist/redux-persist.js
@@ -1,5 +1,5 @@
 import { persistStore, persistReducer } from 'redux-persist'
-import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+import storage from 'redux-persist/lib/storage' 
  
 import todoManagerReducer from '../../reducer/todoManagerReducer'
  
